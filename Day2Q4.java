f32096db8a503a020b9fc6d24f6aec578c01dd7d
Riyad193 <129787687+Riyad193@users.noreply.github.com>
2023-04-06 19:56:11 +0300

    Add files via upload

---
+++ Day2Q4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3 = input.nextInt();
+
+        int max = num1;
+        if (num2 > max) {
+            max = num2;
+        }
+        if (num3 > max) {
+            max = num3;
+        }
+
+        System.out.println("The largest number is: " + max);
+    }
+}
